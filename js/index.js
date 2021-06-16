7d61fe5aaccb7bbd89c3a5c71ce06a2efd602f4d
md ashif reza <mdashifreza111@gmail.com>
2021-06-16 16:49:46 +0530

    my wow commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// console.log(location.href)
+
+// let hoverclass = document.querySelectorAll(".hoverclass");
+
+// hoverclass.forEach((elem, index) => {
+//     elem.addEventListener("click", () => {
+//         if (location.href === "http://127.0.0.1:5500/html/home.html") {
+//             elem.classList.add('hidehoverclass')
+//         }
+//         else {
+//             elem.classList.remove("hidehoverclass")
+//         }
+//     })
+// })
+
+// console.log(hoverclass)
